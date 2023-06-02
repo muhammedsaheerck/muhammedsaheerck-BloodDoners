7bb742a0606a147d37aa83f5997165deb9b80ffd
Muhammed Saheer ck <saheerchelakkodan@gmail.com>
2023-06-02 16:09:38 +0530

    sigIn screen  updated

--- lib/Screens/screen_signin.dart
+++ lib/Screens/screen_signin.dart
@@ -28,52 +28,100 @@ class _ScreenSignInState extends State<ScreenSignIn> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
-                decoration: const InputDecoration(hintText: "Email"),
-                controller: emilController,
-                validator: (value) {
-                  if (value!.isEmpty || !value.contains("@")) {
-                    return "Please enter valid email";
-                  } else {
-                    return null;
-                  }
-                },
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Email", border: InputBorder.none),
+                    controller: emilController,
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains("@")) {
+                        return "Please enter valid email";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
               ),
               const SizedBox(
                 height: 10,
               ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: "Password"),
-                controller: passwordControoller,
-                validator: (value) {
-                  if (value!.isEmpty || value.length < 6) {
-                    return "password lenthe minimum 6";
-                  } else {
-                    return null;
-                  }
-                },
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Password", border: InputBorder.none),
+                    controller: passwordControoller,
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 6) {
+                        return "password lenthe minimum 6";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "signup");
+                      },
+                      child: const Text("create account")),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: 300,
+                child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+
+                        AuthServices.signinUser(emilController.text,
+                            passwordControoller.text, context);
+                      }
+                    },
+                    child: const Text("SignIn")),
               ),
               const SizedBox(
                 height: 20,
               ),
-              TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "signup");
-                  },
-                  child: const Text("create account")),
+              const Text("OR"),
               const SizedBox(
-                height: 30,
+                height: 20,
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-
-                      AuthServices.signinUser(emilController.text,
-                          passwordControoller.text, context);
-                    }
-                  },
-                  child: const Text("SignIn"))
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () => AuthServices().googleSignIn(context),
+                    child: SizedBox(
+                      child: Image.asset(
+                        "asset/download.png",
+                        height: 60,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    // onTap: () => AuthServices().googleSignIn(context),
+                    child: SizedBox(
+                      child: Image.asset(
+                        "asset/download (1).png",
+                        height: 50,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
